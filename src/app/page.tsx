3ecc0d0f4d9e0e4f73a9c0254b14c6fd63d554a7
ayuukumakuma <ayuu.kumakuma@gmail.com>
2024-09-07 21:24:23 +0900

    add framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import styles from "./page.module.css";
+
 export default function Home() {
 	return (
-		<div>
+		<div className={styles.root}>
 			<h1>Home</h1>
 			<p>
 				Welcome to your new Next.js app! This is a simple page that you can
