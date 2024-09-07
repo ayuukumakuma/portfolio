603741f7073ee5c326a61521a6fa48534c970d00
ayuukumakuma <ayuu.kumakuma@gmail.com>
2024-09-07 18:37:37 +0900

    test lefthook

--- src/app/page.module.css.d.ts
+++ src/app/page.module.css.d.ts
@@ -1,28 +1,5 @@
 declare const styles:
-  & Readonly<{ "page": string }>
-  & Readonly<{ "page": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "ctas": string }>
-  & Readonly<{ "ctas": string }>
-  & Readonly<{ "primary": string }>
-  & Readonly<{ "secondary": string }>
-  & Readonly<{ "footer": string }>
-  & Readonly<{ "footer": string }>
-  & Readonly<{ "footer": string }>
-  & Readonly<{ "primary": string }>
-  & Readonly<{ "secondary": string }>
-  & Readonly<{ "footer": string }>
-  & Readonly<{ "page": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "main": string }>
-  & Readonly<{ "ctas": string }>
-  & Readonly<{ "ctas": string }>
-  & Readonly<{ "secondary": string }>
-  & Readonly<{ "footer": string }>
-  & Readonly<{ "logo": string }>
+  & Readonly<{ "root": string }>
 ;
 export default styles;
 //# sourceMappingURL=./page.module.css.d.ts.map
