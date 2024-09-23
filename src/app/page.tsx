e30a03859b235a9bb67ac20b726f12c91b385a8a
ayuukumakuma <ayuu.kumakuma@gmail.com>
2024-09-23 17:03:29 +0900

    change to static export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
 "use client";
-import { motion, useScroll } from "framer-motion";
-import styles from "./page.module.css";
 
 export default function Home() {
-	const { scrollYProgress } = useScroll();
 	return (
-		<div className={styles.root}>
-			<motion.div
-				className={styles.progressBar}
-				style={{ scaleX: scrollYProgress }}
-			/>
-			<div className={styles.scrollContainer}>
-				<section className={styles.scrollItem}>Section 1</section>
-				<section className={styles.scrollItem}>Section 2</section>
-				<section className={styles.scrollItem}>Section 3</section>
-				<section className={styles.scrollItem}>Section 4</section>
-			</div>
+		<div>
+			<h1>Home</h1>
+			<p>Welcome to the home page.</p>
 		</div>
 	);
 }
