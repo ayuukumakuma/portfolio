eb7424f14f9aa09d2b78592c560247ce5b6ae1fb
nasuno.ayumu <67136466+ayuukumakuma@users.noreply.github.com>
2024-09-23 00:49:00 -0700

    Delete stylelint (#10)
    * stylelintを��てる
    
    * lefthookの調整

--- src/app/page.module.css.d.ts
+++ src/app/page.module.css.d.ts
@@ -1,5 +1,8 @@
 declare const styles:
   & Readonly<{ "root": string }>
+  & Readonly<{ "scrollContainer": string }>
+  & Readonly<{ "scrollItem": string }>
+  & Readonly<{ "progressBar": string }>
 ;
 export default styles;
 //# sourceMappingURL=./page.module.css.d.ts.map
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+"use client";
+import { motion, useScroll } from "framer-motion";
 import styles from "./page.module.css";
 
 export default function Home() {
+	const { scrollYProgress } = useScroll();
 	return (
 		<div className={styles.root}>
-			<h1>Home</h1>
-			<p>
-				Welcome to your new Next.js app! This is a simple page that you can
-				start editing.
-			</p>
+			<motion.div
+				className={styles.progressBar}
+				style={{ scaleX: scrollYProgress }}
+			/>
+			<div className={styles.scrollContainer}>
+				<section className={styles.scrollItem}>Section 1</section>
+				<section className={styles.scrollItem}>Section 2</section>
+				<section className={styles.scrollItem}>Section 3</section>
+				<section className={styles.scrollItem}>Section 4</section>
+			</div>
 		</div>
 	);
 }
