f4cc64bf6d249b7bd128da619492913896002194
nasuno.ayumu <67136466+ayuukumakuma@users.noreply.github.com>
2024-09-23 01:55:03 -0700

    Revert "change to static export (#13)" (#19)
    This reverts commit b1a36bff036f77f82265ae12bd8d438a5b98d4d2.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 "use client";
+import { motion, useScroll } from "framer-motion";
+import styles from "./page.module.css";
 
 export default function Home() {
+	const { scrollYProgress } = useScroll();
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>Welcome to the home page.</p>
+		<div className={styles.root}>
+			<motion.div
+				className={styles.progressBar}
+				style={{ scaleX: scrollYProgress }}
+			/>
+			<div className={styles.scrollContainer}>
+				<section className={styles.scrollItem}>Section 1</section>
+				<section className={styles.scrollItem}>Section 2</section>
+				<section className={styles.scrollItem}>Section 3</section>
+				<section className={styles.scrollItem}>Section 4</section>
+			</div>
 		</div>
 	);
 }
